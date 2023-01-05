93898b10fd07ffe26e29c62ae1495906152bc149
HangLe23 <20521286@gm.uit.edu.vn>
2023-01-05 16:33:57 +0700

    UPdate

---
+++ app/src/main/java/com/example/nt118_nhom2_trips/CreateNewTrip.java
@@ -0,0 +1,18 @@
+package com.example.nt118_nhom2_trips;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateNewTrip extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_newtrip);
+
+    }
+}
--- app/src/main/java/com/example/nt118_nhom2_trips/MainActivity.java
+++ app/src/main/java/com/example/nt118_nhom2_trips/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.nt118_nhom2_trips;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button create;
+    private ImageButton hotel, tour, yourTrip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        create = (Button) findViewById(R.id.btn_CreateTrip);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iNewActivity = new Intent(MainActivity.this, CreateNewTrip.class);
+                startActivity(iNewActivity);
+            }
+        });
     }
+
 
 }
