a1f01108611b43361f66dd825c01859cd6d0c703
HangLe23 <20521286@gm.uit.edu.vn>
2023-01-07 21:32:03 +0700

    UPdate

---
+++ app/src/main/java/com/example/nt118_nhom2_trips/NewActivity.java
@@ -0,0 +1,33 @@
+package com.example.nt118_nhom2_trips;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewActivity extends AppCompatActivity {
+    Button btn_Sign_in,btn_sign_up;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+        btn_Sign_in = (Button) findViewById(R.id.btn_Sign_in);
+        btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
+        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewActivity.this, SignUp.class));
+
+            }
+        });
+        btn_Sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewActivity.this, SignIn.class));
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/nt118_nhom2_trips/SignIn.java
@@ -0,0 +1,16 @@
+package com.example.nt118_nhom2_trips;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
