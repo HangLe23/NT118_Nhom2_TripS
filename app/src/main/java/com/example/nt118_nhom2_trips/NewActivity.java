66cbf0fd4c6b8a55338499be25f2e36299cf5d2d
HangLe23 <20521286@gm.uit.edu.vn>
2023-01-12 10:36:28 +0700

    UPdate

--- app/src/main/java/com/example/nt118_nhom2_trips/NewActivity.java
+++ app/src/main/java/com/example/nt118_nhom2_trips/NewActivity.java
@@ -2,10 +2,14 @@ package com.example.nt118_nhom2_trips;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class NewActivity extends AppCompatActivity {
     private Button btn_Sign_in,btn_sign_up;
@@ -13,8 +17,29 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
-        btn_Sign_in = (Button) findViewById(R.id.btn_Sign_in);
-        btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
+
+        /*Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user == null){
+                    Intent intent = new Intent(NewActivity.this, NewActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent = new Intent(NewActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                finish();
+            }
+        });*/
+        findViewByIds();
+        initListener();
+
+    }
+
+    private void initListener() {
         btn_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +55,10 @@ public class NewActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void findViewByIds() {
+        btn_Sign_in = (Button) findViewById(R.id.btn_Sign_in);
+        btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
     }
 }
