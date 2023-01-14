c6beabe75a5c16ab40f43b1bfc03927ee3d1b178
HangLe23 <20521286@gm.uit.edu.vn>
2023-01-14 17:16:02 +0700

    UPdate

--- app/src/main/java/com/example/nt118_nhom2_trips/ui/changepassword/ChangPasswordFragment.java
+++ app/src/main/java/com/example/nt118_nhom2_trips/ui/changepassword/ChangPasswordFragment.java
@@ -1,19 +1,86 @@
 package com.example.nt118_nhom2_trips.ui.changepassword;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.nt118_nhom2_trips.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChangPasswordFragment extends Fragment {
+    private View view;
+    private EditText newPass, oldPass, confirmPass;
+    private Button btnChangePass;
+    private ProgressDialog progressDialog;
+    @NonNull
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_changepass, container, false);
-        return v;
+        view = inflater.inflate(R.layout.fragment_changepass, container, false);
+        findViewByIds();
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickChangePass();
+            }
+        });
+        return view;
+    }
+
+    private void onClickChangePass() {
+        String NewPass = newPass.getText().toString().trim();
+        progressDialog.show();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.updatePassword(NewPass)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "User password updated.", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        } else {
+                            //
+                        }
+                    }
+                });
+    }
+
+    private void findViewByIds(){
+        progressDialog = new ProgressDialog(getActivity());
+        newPass = view.findViewById(R.id.et_newpassword);
+        oldPass = view.findViewById(R.id.et_oldpassword);
+        confirmPass = view.findViewById(R.id.et_confirmpass);
+        btnChangePass = view.findViewById(R.id.btn_changepass);
+    }
+    private void reAuthenticate(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        AuthCredential credential = EmailAuthProvider
+                .getCredential("user@example.com", "password1234");
+
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            onClickChangePass();
+                        } else {
+
+                        }
+                    }
+                });
     }
 }
